c3abad131d42a86536a644e79cc911b5ea4c1a78
maytee saechoa <74194143+dahyunsz@users.noreply.github.com>
2021-03-08 23:09:09 +0700

    Add files via upload

--- feature/getUser.php
+++ feature/getUser.php
@@ -1,1 +1,29 @@
-
+<?php 
+    include "connect.php";
+    $stmt = $pdo->prepare("SELECT * FROM user");
+    $stmt->execute();
+    while($row = $stmt->fetch()){
+        echo '
+            <div class="section">
+                <ul>
+                    <li>
+                        <div class="boxUserList">
+                            <div class="box">
+                                <a href="user.php?username='.$row["username"].'">
+                                    <img src="images/userpicture.png" alt="Img" height="94" width="90">
+                                </a>
+                            </div>
+                            <p>
+                                Firstname: '.$row["firstname"].'<br>
+                                Lastname: '.$row["lastname"].'<br>
+                                Email: '.$row["email"].'
+                                <a class="more" href="user.php?username='.$row["username"].'">ตรวจสอบ</a>
+                            </p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            ';
+        
+    }
+?>
